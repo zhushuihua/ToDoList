738bedbd54e59795d00aeb95aa533edaa0b377ff
Joseph <null>
2018-11-21 16:42:22 +0800

    complete UserDefaults based todolist

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    let defaults = UserDefaults.standard
+    var items = [String]()
+    var dones = [Bool]()
+    let itemsKey = "itemsKey"
+    let doneKey = "doneKey"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: itemsKey) as? [String]
+        {
+            self.items = items
+        }
+        if let dones = defaults.array(forKey: doneKey) as? [Bool]
+        {
+            self.dones = dones
+        }
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first)
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        cell.detailTextLabel?.text = items[indexPath.row]
+        let done = dones[indexPath.row]
+        cell.accessoryType = done ? .checkmark : .none
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        dones[indexPath.row] = !dones[indexPath.row]
+        cell?.accessoryType = dones[indexPath.row] ? .checkmark : .none
+        defaults.set(dones, forKey: doneKey)
+    }
+    
+    @IBAction func addItemButtonPressed(_ sender: UIBarButtonItem) {
+        var textFieldx:UITextField!
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textFieldx = textField
+            textField.placeholder = "Add New Item Here"
+        }
+        let addAction = UIAlertAction(title: "Add", style: .default) { action in
+            let str = textFieldx.text!
+            self.items.append(str)
+            self.dones.append(false)
+            self.defaults.set(self.items, forKey:self.itemsKey)
+            self.defaults.set(self.dones, forKey:self.doneKey)
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
