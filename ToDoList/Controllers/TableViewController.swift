0ff047ed4479cd5af48e1c70689f0bf4d588cf3f
Joseph <null>
2018-11-21 17:20:29 +0800

    Complete plist based save operation, add finish time.

--- ToDoList/Controllers/TableViewController.swift
+++ ToDoList/Controllers/TableViewController.swift
@@ -10,20 +10,12 @@ import UIKit
 
 class TableViewController: UITableViewController {
     let defaults = UserDefaults.standard
-    var items = [String]()
-    var dones = [Bool]()
+    var items = [Item]()
     let itemsKey = "itemsKey"
-    let doneKey = "doneKey"
+    let pListPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: itemsKey) as? [String]
-        {
-            self.items = items
-        }
-        if let dones = defaults.array(forKey: doneKey) as? [Bool]
-        {
-            self.dones = dones
-        }
+        loadList()
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -31,17 +23,34 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        cell.detailTextLabel?.text = items[indexPath.row]
-        let done = dones[indexPath.row]
+        let item = items[indexPath.row]
+        let done = item.done
         cell.accessoryType = done ? .checkmark : .none
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        if let date = item.finishDate
+        {
+            cell.detailTextLabel?.text = "\(date)"
+        }
+        else
+        {
+            cell.detailTextLabel?.text = "Ongoing"
+        }
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
-        dones[indexPath.row] = !dones[indexPath.row]
-        cell?.accessoryType = dones[indexPath.row] ? .checkmark : .none
-        defaults.set(dones, forKey: doneKey)
+        items[indexPath.row].done = !items[indexPath.row].done
+        cell?.accessoryType = items[indexPath.row].done ? .checkmark : .none
+        items[indexPath.row].finishDate = items[indexPath.row].done ? Date() : nil
+        if let date = items[indexPath.row].finishDate
+        {
+            cell?.detailTextLabel?.text = "\(date)"
+        }
+        else
+        {
+            cell?.detailTextLabel?.text = "Ongoing"
+        }
+        saveList()
     }
     
     @IBAction func addItemButtonPressed(_ sender: UIBarButtonItem) {
@@ -53,14 +62,32 @@ class TableViewController: UITableViewController {
         }
         let addAction = UIAlertAction(title: "Add", style: .default) { action in
             let str = textFieldx.text!
-            self.items.append(str)
-            self.dones.append(false)
-            self.defaults.set(self.items, forKey:self.itemsKey)
-            self.defaults.set(self.dones, forKey:self.doneKey)
+            let item = Item()
+            item.title = str
+            self.items.append(item)
             self.tableView.reloadData()
+            self.saveList()
         }
         alert.addAction(addAction)
         self.present(alert, animated: true, completion: nil)
+    }
+    private func loadList(){
+        let decoder = PropertyListDecoder()
+        if let data = try? Data(contentsOf: pListPath!)
+        {
+          if let  items = try? decoder.decode([Item].self, from: data)
+            {
+                self.items = items
+                tableView.reloadData()
+            }
+        }
+    }
+    private func saveList(){
+        let encoder = PropertyListEncoder()
+        if let data = try? encoder.encode(items)
+        {
+            try? data.write(to: pListPath!)
+        }
     }
 }
 
---
+++ ToDoList/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Shuihua Zhu on 21/11/18.
+//  Copyright © 2018 UMA Mental Arithmetic. All rights reserved.
+//
+
+import Foundation
+class Item:Codable{
+    var title:String = ""
+    var done:Bool = false
+    var finishDate:Date? = nil
+}
